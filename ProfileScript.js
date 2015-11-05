14faf7826b584db3414886e074d30d74ff83f425
kjeyakumar <kjeyakum@gmail.com>
2015-11-04 22:34:53 -0500

    Added buttons as header
    Added buttons as header and messages to be hidden on click

--- ProfileScript.js
+++ ProfileScript.js
@@ -1,3 +1,21 @@
 /**
  * 
  */
+
+function changeImage()
+{
+	document.getElementById("profilepicture").style.height="35%";
+	document.getElementById("profilepicture").style.width="35%";
+	return true;
+}
+function toggleMe(a){
+	var e=document.getElementById(a);
+	if(!e)return true;
+	if(e.style.display=="none"){
+	e.style.display="block"
+	}
+	else{
+	e.style.display="none"
+	}
+	return true;
+}
